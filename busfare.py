f495b22b475dd0fba9a7bf27bff67fe548928bb9
CoolstaConnor <160691687+CoolstaConnor@users.noreply.github.com>
2024-08-07 08:46:43 +0000

    copy paste

---
+++ busfare.py
@@ -0,0 +1,3 @@
+stopat = int(input("What number stop are you at?"))
+stopgo = int(input("What is the stop you want to go to?"))
+print(f"The fare from Stop {stopat} to Stop {stopgo} is ${(stopgo - stopat) * 2.50}")
