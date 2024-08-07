b9334d80008e9e1a3dcfb9a3d992e2cb0d45c61f
CoolstaConnor <160691687+CoolstaConnor@users.noreply.github.com>
2024-08-07 10:39:23 +0000

    e

--- guessnum.py
+++ guessnum.py
@@ -1,14 +1,10 @@
 import random
-randomnum = random.randint(1, 26)
-alphabet = {
-    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
-    'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
-    't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
-}
-guesscount = 0
-while True:
-    letter = input("Enter a letter: ").lower()
-    guesscount += 1
-    if alphabet.get(letter) == randomnum:
-        break
-print(f"You guessed {guesscount} times.")
+number = random.randint(1, 100)
+guess = int(input("Enter your guess: "))
+while guess != number:
+    if guess < number:
+        print("Your guess was too low.")
+    elif guess > number:
+        print("Your guess was too high.")
+    guess = int(input("Enter your guess: "))
+print("Correct!")
